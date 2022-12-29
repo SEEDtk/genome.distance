660565f522cd18303744c541a85280667f7829cd
Bruce Parrello <bparrello@figresearch.com>
2022-12-29 07:50:50 -0600

    Added ability to query genome ID.

--- src/main/java/org/theseed/genome/distance/methods/Measurer.java
+++ src/main/java/org/theseed/genome/distance/methods/Measurer.java
@@ -7,19 +7,31 @@ import org.theseed.genome.Genome;
 
 /**
  * This is the base class for measurement objects that can be used to apply a single
- * measurement method to a single genome.  It has no real methods or fields, only a
- * constructor.
+ * measurement method to a single genome.
  *
  * @author Bruce Parrello
  *
  */
 public abstract class Measurer {
 
+    // FIELDS
+    /** ID of the genome being measured */
+    private String genomeId;
+
     /**
      * Construct a measurement structure for a single genome.
      *
      * @param genome	genome to use
      */
-    public Measurer(Genome genome) { }
+    public Measurer(Genome genome) {
+        this.genomeId = genome.getId();
+    }
+
+    /**
+     * @return the ID of the genome being measured
+     */
+    public String getGenomeId() {
+        return this.genomeId;
+    }
 
 }
